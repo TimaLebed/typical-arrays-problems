87d826cbcb98cb99e091f192b35614b8e964db8a
TimaLebed <timalebedevskiy@mail.ru>
2020-03-02 17:58:29 +0300

    refactor: move const up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+const isValid = arr => arr && arr.length;
+
 exports.min = arr => isValid(arr) ? Math.min(...arr) : 0;
 
 exports.max = arr => isValid(arr) ? Math.max(...arr) : 0;
 
 exports.avg = arr => isValid(arr) ? arr.reduce((acc, el) => acc + el, 0) / arr.length : 0;
-
-const isValid = arr => arr && arr.length;
